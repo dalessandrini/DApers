46caf4cd91bcef0a9e74f44cba9844acd0c2b137
dalessandrini <daniel.alessandrini@gmail.com>
2022-01-14 19:17:36 -0300

    Cambiamos varias cosas en limpieza_TS_TT, actualizamos tablas de equivalencias, probamos

---
+++ R/DApers.R
@@ -0,0 +1,8 @@
+#' DApers: paquete para generar registros personales adecuados para analisis
+#'
+#' Tiene unas pocas funciones por ahora; la idea es que siga creciendo y que
+#' sirva de inspiracion para otros paquetes que vamos a armar.
+#'
+#'@docType package
+#'@name DApers
+NULL
--- R/limpieza_TS_TT.R
+++ R/limpieza_TS_TT.R
@@ -28,74 +28,16 @@ importar_tt <- function(archivo_csv,...){
   nmb <- split[[1]]
   tt <- data.frame(df_tt[-1,],stringsAsFactors=FALSE)
   colnames(tt) <- nmb
+  names(tt) <- sub(" ","",names(tt)) #saco espacios del principio de cada columna
+  names(tt) <- gsub(" ",".",names(tt)) #cambio espacios por puntos para hms_fch()
+  names(tt) <- gsub("\"","",names(tt)) #me agrega "" en nombres! Las pelamos
   tt[,9] <- as.character(tt[,9]) #$` COMMENTS` list a character
+  #No sabemos porque se agregan comillas dobles, las sacamos
+  tt$ACTIVITY.NAME <- gsub("\"","",tt$ACTIVITY.NAME) #noquote(tt$ACTIVITY.NAME)
+  tt$COMMENTS <- gsub("\"","",tt$COMMENTS)
   tt[which(tt[,9]=="character(0)"),9] <- NA
   return(tt)
 }
-
-
-### FUNCION etiq_rp 15/01/21
-
-#' Genera tabla con formato TS
-#'
-#' Con salida de importar_tt() se crea nuevo data.frame ajustado a formato TS
-#'
-#' Es la parte 2 del proceso importar_tt() >> etiq_rp(2): crear RP20xx
-#'
-#' @param datos algo
-#' @param tablaEtiq Tabla de Equivalencias de etiquetas TS-TT
-#' @param nombre Nombre al nuevo data.frame
-#' @param exportar (TRUE por defecto) si queres exportar data.frame resultante
-#' @export
-
-etiq_rp <- function(datos,tablaEtiq,nombre="",exportar=TRUE){
-  rp <- datos
-  if(is.null(tablaEtiq))
-    stop("NO se puede seguir sin tabla de equivalencias entre etiquetas TS-TT")
-  rp2 <- merge(rp,tablaEtiq[,1:7],by="Etiquetas",all.x=TRUE,all.y=FALSE,sort=FALSE)
-  #Ordenamos por fecha ascendente
-  rp2 <- rp2[,c(2,9:13,8,5,3:4,1)]
-  rp2 <- rp2[order(rp2[,1]),]
-  #Retocamos fecha como antes
-  fch <- strftime(rp2$Fecha)
-  #Mes
-  ms <- strftime(fch,format='%b')
-  rp2$Mes <- ifelse(ms=="ene","A.Ene",
-              ifelse(ms=="feb","B.Feb",
-              ifelse(ms=="mar","C.Mar",
-              ifelse(ms=="abr","D.Abr",
-              ifelse(ms=="may","E.May",
-              ifelse(ms=="jun","F.Jun",
-              ifelse(ms=="jul","G.Jul",
-              ifelse(ms=="ago","H.Ago",
-              ifelse(ms=="set","I.Set", #OJO con el LOCALE (si es UY es SETiembre!!)
-              ifelse(ms=="oct","J.Oct",
-              ifelse(ms=="nov","K.Nov",
-              ifelse(ms=="dic","L.Dic",NA))))))))))))
-  #Semana
-  sm <- as.numeric(strftime(fch,format='%U'))
-  rp2$Semana <- ifelse(sm<9,paste0("Sem0",sm+1),paste0("Sem",sm+1)) #valido pa 2018 (en 2017 sacar el '+1')
-  #DdS
-  dd <- strftime(fch,format='%a')
-  rp2$DdS <- ifelse(dd=="dom","a.dom",
-              ifelse(dd=="lun","b.lun",
-              ifelse(dd=="mar","c.mar",
-              ifelse(dd=="mié","d.mie",
-              ifelse(dd=="jue","e.jue",
-              ifelse(dd=="vie","f.vie",
-              ifelse(dd=="sáb","g.sab",NA)))))))
-  #Cambiamos nombre cols y reordenamos:
-  names(rp2)[c(5:10)] <- c("Sub_espacio","Sub_subespacio","Descripcion", "Tiempo","HoraInicio","HoraFin")
-  rp2 <- rp2[c("Mes","Semana","DdS","Fecha","TA","Actividad","Espacio","Sub_espacio","Sub_subespacio",
-               "Descripcion","Tiempo","HoraInicio","HoraFin","Etiquetas")]
-  # Paso 5: exportar archivo para guardar en GSS (max info, min t perdido!)
-  if(exportar){
-    write.table(rp2,file=paste0(nombre,".txt"),sep="\t")
-  }
-  # Final: devolveme rp2 pa trabajar
-  return(rp2)
-}
-
 
 ### FUNCION hms_fch para pasar lectura datos TT>>TS (corregida 06/04/20) ###
 
@@ -112,7 +54,7 @@ etiq_rp <- function(datos,tablaEtiq,nombre="",exportar=TRUE){
 #' @param exportar (TRUE por defecto) si queres exportar data.frame resultante
 #' @export
 
-hms_fch <- function(df_tt,formatoFecha='%d/%m/%y',formatoTS=2,exportar=TRUE){
+hms_fch <- function(df_tt,formatoFecha='%d/%m/%Y',formatoTS=2,exportar=TRUE){
   #formatoTS: 1:version <2019, 2:version>2019 (x defecto)
   a <- df_tt
   # Parte 1: Funcion 'hms': entra df TT, sale df TT+ columna 'DURATIONrec' con horas en formato 'H.xx'
@@ -134,20 +76,23 @@ hms_fch <- function(df_tt,formatoFecha='%d/%m/%y',formatoTS=2,exportar=TRUE){
   a$Fch <- fch_ini
   a$HrIni <- coment_hm_ini
   a$HrFin <- coment_hm_fin
-  a$coment_hm <- ifelse(a$COMMENTS=="",coment_hm,a$COMMENTS)
-  # Parte 3: formatear estilo TS pero sacando tambien la info desde ACTIVITY.NAME
+  a$coment_hm <- ifelse(is.na(a$COMMENTS),coment_hm,a$COMMENTS) #cambiado 14/01/22
+  # Parte 3: formatear estilo TS pero sacando tambien la info desde ACTIVITY.NAME (modif 14/01/22)
   a$proye <- factor(with(a,ifelse(ACTIVITY.NAME %in% c("ESIN-R.impl","ESRE-DAb","ESTE-Adm","ESTE-Bib","ESTE_Reu"),"Estudio-Tesis",
                 ifelse(ACTIVITY.NAME %in% c("ESMA-TM"),"Estudio-Materias",
                 ifelse(ACTIVITY.NAME %in% c("OH-Auto","OH-Coci","OH-CoFe","OH-Ctas","OH-LiLR","OH-Limp","OH-Mand","OH-OrgL","OH-ObjC"),"Otros-Hogar",
                 ifelse(ACTIVITY.NAME %in% c("FA-CuiF","FA-DocF","FA-VEFP","FA-ApyF","FA-EveF"),"Familia",
-                ifelse(ACTIVITY.NAME %in% c("OP-DocP","OP-Mand","OP-Salu","OP-CuiP","OP-HTNI","OP-ObjP","OP-Plan","OP-SaCt","OP-Varios"),"Otros-Personales",
+                ifelse(ACTIVITY.NAME %in% c("OP-DocP","OP-Mand","OP-Salu","OP-CuiP","OP-HNTI","OP-ObjP","OP-Plan","OP-SaCt","OP-Varios","OP-DebC"),"Otros-Personales",
                 ifelse(ACTIVITY.NAME %in% c("TL-EC","TL-Otr","TL-ReAm","TL-ReFa","TL-Sali","TL-Siestas","TL-Viaj"),"Tiempo_Libre",
                 ifelse(ACTIVITY.NAME %in% c("Trp-Bus","Trp-Priv","Trp-Taxi","Trp-Intl"),"Transporte",
                 #Agregado 13/01/21 por no estar antes en TT
-                ifelse(ACTIVITY.NAME %in% c("TRFG-ACUy","TRFG-PW"),"Trabajo-FGR",
+                ifelse(ACTIVITY.NAME %in% c("TRFG-ACUy","TRFG-Cortos","TRFG-PSV","TRFG-PW","TRFG-SVD"),"Trabajo-FGR",
                 ifelse(ACTIVITY.NAME %in% c("TRUE-Ap","TRUE-CU","TRUE-In","TRUE-Ot","TRUE-Ge","TRUE-Gral","TRUE-Re"),"Trabajo-UEFI",
                 ifelse(ACTIVITY.NAME %in% c("TRNF"),"Trabajo-NoFacturable",
-                ifelse(ACTIVITY.NAME %in% c("TRAS"),"Trabajo-Asesoramiento","__Completar__")))))))))))))
+                ifelse(ACTIVITY.NAME %in% c("TRAS"),"Trabajo-Asesoramiento",
+                #Nuevos desde 14/01/22
+                ifelse(ACTIVITY.NAME %in% c("DIEC","DICC"),"Deporte",
+                ifelse(ACTIVITY.NAME %in% c("TRIM"),"Trabajo-IM","__Completar__")))))))))))))))
   # Parte 4: reordenamos columnas con nombres correctos (y agregando columnas vacias para que se parezca a TS)
   a$DurRel <- a$DURATIONdec #iguales pa que no se arme lio
   a$GastosDscr <- a$GastosNoPag <- a$GastosPag <- a$Gastos <- a$PausasDscr <- a$Pausas <- a$Salario <- 0
@@ -173,6 +118,70 @@ hms_fch <- function(df_tt,formatoFecha='%d/%m/%y',formatoTS=2,exportar=TRUE){
   }
   else return(nuevo_a) #x otros formatos modificar esta parte
 }
+
+
+### FUNCION etiq_rp 15/01/21
+
+#' Genera tabla con formato TS
+#'
+#' Con salida de importar_tt() se crea nuevo data.frame ajustado a formato TS
+#'
+#' Es la parte 2 del proceso importar_tt() >> etiq_rp(2): crear RP20xx
+#'
+#' @param datos algo
+#' @param tablaEtiq Tabla de Equivalencias de etiquetas TS-TT
+#' @param nombre Nombre al nuevo data.frame
+#' @param exportar (TRUE por defecto) si queres exportar data.frame resultante
+#' @export
+
+etiq_rp <- function(datos,tablaEtiq,nombre="",exportar=TRUE){
+  rp <- datos
+  if(is.null(tablaEtiq))
+    stop("NO se puede seguir sin tabla de equivalencias entre etiquetas TS-TT")
+  rp2 <- merge(rp,tablaEtiq[,1:7],by="Etiquetas",all.x=TRUE,all.y=FALSE,sort=FALSE)
+  #Ordenamos por fecha ascendente
+  rp2 <- rp2[,c(2,10:14,8,5,3:4,1)] #cambio 14/01/22 (antes 2,9:13,8,5,3:4,1)
+  rp2 <- rp2[order(rp2[,1]),]
+  #Retocamos fecha como antes
+  fch <- strftime(rp2$Fch)
+  #Mes
+  ms <- strftime(fch,format='%b')
+  rp2$Mes <- ifelse(ms=="ene","A.Ene",
+              ifelse(ms=="feb","B.Feb",
+              ifelse(ms=="mar","C.Mar",
+              ifelse(ms=="abr","D.Abr",
+              ifelse(ms=="may","E.May",
+              ifelse(ms=="jun","F.Jun",
+              ifelse(ms=="jul","G.Jul",
+              ifelse(ms=="ago","H.Ago",
+              ifelse(ms %in% c("set","sep","sept"),"I.Set", #OJO con el LOCALE (si es UY es SETiembre!!)
+              ifelse(ms=="oct","J.Oct",
+              ifelse(ms=="nov","K.Nov",
+              ifelse(ms=="dic","L.Dic",NA))))))))))))
+  #Semana
+  sm <- as.numeric(strftime(fch,format='%U'))
+  rp2$Semana <- ifelse(sm<9,paste0("Sem0",sm+1),paste0("Sem",sm+1)) #valido pa 2018 (en 2017 sacar el '+1')
+  #DdS
+  dd <- strftime(fch,format='%a')
+  rp2$DdS <- ifelse(dd=="dom","a.dom",
+              ifelse(dd=="lun","b.lun",
+              ifelse(dd=="mar","c.mar",
+              ifelse(dd=="mi\u00E9","d.mie",
+              ifelse(dd=="jue","e.jue",
+              ifelse(dd=="vie","f.vie",
+              ifelse(dd=="s\u00E1b","g.sab",NA)))))))
+  #Cambiamos nombre cols y reordenamos:
+  names(rp2)[c(1,5:10)] <- c("Fecha","Sub_espacio","Sub_subespacio","Descripcion", "Tiempo","HoraInicio","HoraFin")
+  rp2 <- rp2[c("Mes","Semana","DdS","Fecha","TA","Actividad","Espacio","Sub_espacio","Sub_subespacio",
+               "Descripcion","Tiempo","HoraInicio","HoraFin","Etiquetas")]
+  # Paso 5: exportar archivo para guardar en GSS (max info, min t perdido!)
+  if(exportar){
+    write.table(rp2,file=paste0(nombre,".txt"),sep="\t",row.names=FALSE)
+  }
+  # Final: devolveme rp2 pa trabajar
+  return(rp2)
+}
+
 
 #-------------------------------------------------------------------------------#
 
